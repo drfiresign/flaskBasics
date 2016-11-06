56365f572955c2ed49445697c2308b38a51223b6
Dylan Cascio <Dcascio73@gmail.com>
2016-11-05 19:50:14 -0700

    Added view to multiply numbers
    Same as the previous update, I've changed the operation from addition to
    multiplication.

--- simple_app.py
+++ simple_app.py
@@ -17,4 +17,12 @@ def add(num1, num2):
     return "{} + {} = {}".format(num1, num2, num1 + num2)
 
 
+@app.route('/multiply/<float:num1>/<float:num2>')
+@app.route('/multiply/<int:num1>/<int:num2>')
+@app.route('/multiply/<float:num1>/<int:num2>')
+@app.route('/multiply/<int:num1>/<float:num2>')
+def multiply(num1=5, num2=5):
+    return "{} * {} = {}".format(num1, num2, num1 * num2)
+
+
 app.run(debug=True, port=8000, host='0.0.0.0')
