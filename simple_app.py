27828a075dec44765c988d574c090422b44388b5
Dylan Cascio <Dcascio73@gmail.com>
2016-11-05 19:57:22 -0700

    Added Template folder and our first Template
    Created new template folder, added the html for the /add page

--- simple_app.py
+++ simple_app.py
@@ -14,7 +14,17 @@ def index(name="Treehouse"):
 @app.route('/add/<float:num1>/<int:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 def add(num1, num2):
-    return "{} + {} = {}".format(num1, num2, num1 + num2)
+    return """
+<!doctype html>
+<html>
+<head><title>Adding!</title></head>
+<body>
+<h1>
+{} + {} = {}
+</h1>
+</body>
+</html>
+""".format(num1, num2, num1 + num2)
 
 
 @app.route('/multiply/<float:num1>/<float:num2>')
@@ -22,7 +32,17 @@ def add(num1, num2):
 @app.route('/multiply/<float:num1>/<int:num2>')
 @app.route('/multiply/<int:num1>/<float:num2>')
 def multiply(num1=5, num2=5):
-    return "{} * {} = {}".format(num1, num2, num1 * num2)
+    return """
+<!doctype html>
+<html>
+<head><title>Adding!</title></head>
+<body>
+<h1>
+{} * {} = {}
+</h1>
+</body>
+</html>
+""".format(num1, num2, num1 * num2)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
