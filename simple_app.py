2be53042eb983fd06b271f6684b8f09755019357
Dylan Cascio <Dcascio73@gmail.com>
2016-11-05 19:43:47 -0700

    Added add view to Flask app
    Added a view to add two numbers together. Created 4 routes to add
    floats and ints together interchangably.

--- simple_app.py
+++ simple_app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Hello from Treehouse!"
+@app.route('/<name>')
+def index(name="Treehouse"):
+    return "Hello from {}!".format(name)
+
+
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+def add(num1, num2):
+    return "{} + {} = {}".format(num1, num2, num1 + num2)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
